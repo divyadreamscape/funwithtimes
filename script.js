b1989cbd85c7a4896aea91d509b79c995ba30e66
Raj Parameswaran <rajkrish@Rajs-MacBook-Air.local>
2023-06-06 16:26:12 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let num1, num2, score = 0;
+
+function newQuestion() {
+    num1 = Math.floor(Math.random() * 10) + 1;
+    num2 = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('question').textContent = `What's ${num1} * ${num2}?`;
+}
+
+document.getElementById('check').addEventListener('click', function() {
+    let answer = document.getElementById('answer').value;
+    if (answer == num1 * num2) {
+        document.getElementById('result').textContent = 'Correct! Good job! 😊';
+        score++;
+    } else {
+        document.getElementById('result').textContent = 'Oops! That’s not correct. Try again. 😔';
+    }
+    document.getElementById('score').textContent = `Score: ${score}`;
+    document.getElementById('answer').value = '';
+    newQuestion();
+});
+
+newQuestion();
+
